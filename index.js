21e6a28b4239de8946c63cbbfde8ee6c78230266
Dragon <36054734+immersivescience@users.noreply.github.com>
2024-11-25 15:34:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+// index.js
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
